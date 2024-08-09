782632ce624f5cc1ad53e6b233e9dba4d30a5d6e
netra2127 <netra200021kcbdr@gmail.com>
2024-08-09 12:58:58 +0100

    node js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 console.log('Hello world from NodeJs.')
 
+const tutorial = require('./tutorial');
+console.log(tutorial.sum(1,1));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject());
 
-
+// const result = tutorial(1,1);
+// console.log("The sum of the two number is : ", result)
 
 
