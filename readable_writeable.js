2faa0452519bd5c32731cf02c6b821cb7e05fc8e
netra2127 <netra200021kcbdr@gmail.com>
2024-08-09 13:52:03 +0100

    node js readable and writeable stream nodejs

--- readable_writeable.js
+++ readable_writeable.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 const readStream = fs.createReadStream('./example2.txt', 'utf8');
+
+const writeStream = fs.createWriteStream('example2.txt');
+
 readStream.on('data', (chunk) => {
-    console.log(chunk);
+    // console.log(chunk);
+    writeStream.write(chunk);
 })
+
+
 
 
 
