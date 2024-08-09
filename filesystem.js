6e5714e844f2d89cb6857f4c18e9e3da727f7ba4
netra2127 <netra200021kcbdr@gmail.com>
2024-08-09 13:27:49 +0100

    filesystem on Node js

--- filesystem.js
+++ filesystem.js
@@ -7,5 +7,43 @@ fs.writeFile('example.txt', "This is an example", (err) =>{
     }
     else{
         console.log("File successfully created.");
+        fs.readFile('example.txt','utf8',(err, file) => {
+            if (err){
+                console.log(err);
+            }
+            else{
+                console.log(file);
+            }
+        });
+    }
+});
+
+// Renaming the file. 
+fs.rename('example.txt', 'example2.txt', (err) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log('successfully renamed the file..');
     }
 })
+
+// Appending the data to the file. 
+fs.appendFile('example2.txt', 'Some data being appended', (err) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log('Successfully appended data to file...');
+    }
+})
+
+// Deleting the file. 
+// fs.unlink('example2.txt', (err) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log('Successfully deleted the file....');
+//     }
+// })
